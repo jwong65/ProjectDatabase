e5755afd55d201220c3d1df9407982c21233e9e4
jwong65 <jason.wong6507@gmail.com>
2023-10-17 16:12:21 -0400

    Added the form box using bootstrap styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='box'>HELLO</div>
+    <div className='box'>
+        <h1>This is a Pokemon selector</h1>
+        <div className='form-group'>
+            <label for='pokemonsearch'>Search for a Pokemon</label>
+            <input type='text' class='form-control' placeholder='Do not put in special characters.'/>
+
+        </div>
+    </div>
   )
 }
