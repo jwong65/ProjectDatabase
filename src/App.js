6202ec460db6704e144fd364101c3752202292c9
jwong65 <jason.wong6507@gmail.com>
2023-10-17 15:44:01 -0400

    removed the deployment and imported bootstrap for the css

--- src/App.js
+++ src/App.js
@@ -3,48 +3,13 @@ import './sass/style.css'
 import { useState } from 'react';
 import Axios from 'axios'
 
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
-  const base_URL = 'https://pokeapi.co/api/v2/pokemon'
-  const [pokemonName, setPokemonName] = useState('')
-
-  const [pokemonInfo, setPokemonInfo]= useState({
-    name: '',
-    img: '',
-    hp: '',
-    attack: '',
-    type:'',
-  })
-  
-  // A use State to see if anything is being searched
-  const [searchEmpty, setSearchEmpty]= useState(false)
-
-  // Function for the button.
-  const searchFunction =()=>{
-    Axios.get(`${base_URL}/${pokemonName}`).then((response)=>
-      setPokemonInfo({name: pokemonName, img: response.data.sprites.front_default, hp: response.data.stats[0].base_stat,
-      attack: response.data.stats[1].base_stat, type:response.data.types[0].type.name}))
-
-      setSearchEmpty(true)
-    }
-//  Things to fix, the captialization of the search and the captialization of the names. Fix for card display, Fix for component searching.
-// Fix for secondary typing search
 return (
-    <div className="App">
-      <div className='box'>
-        <h1>Search Pokemon Name</h1>
-        <input type='text' onChange={(e)=>{setPokemonName(e.target.value)}}></input>
-        <button onClick={searchFunction}>Search</button>
-      </div>
-      {/* <Display/> */}
-      <div>{!searchEmpty ?(<h1>Nothing is chosen</h1>) :(<> <h1>{pokemonInfo.name}</h1>
-        <h2>{pokemonInfo.hp}</h2>
-        <h2>{pokemonInfo.attack}</h2>
-        <img src={pokemonInfo.img} alt='Pokemon image'/>
-        <h2>{pokemonInfo.type}</h2></>
-        )}
-       
-      </div>
-    </div>
+  <>
+  </>
   );
 }
 
--- src/components/Display.js
+++ src/components/Display.js
@@ -8,20 +8,6 @@ const base_URL = 'https://pokeapi.co/api/v2/pokemon'
 const [pokedata, setData] = useState([]);
 const [pokemonName, setName]= useState()
 
-const fetchInfo =() =>{
-    console.log(`${base_URL}/${pokemonName}`)
-  fetch(`${base_URL}/${pokemonName}?limit=2offset=10`)
-  .then((res)=>res.json())
-  .then((json)=> {
-    console.log(json)
-    setData(json)
-  })
-}
-const handleSubmit = (e)=>{
-    e.preventDefault()
-    console.log(pokemonName)
-    fetchInfo();
-}
   return (
     <div className='display'>
         <form onSubmit={handleSubmit}>
@@ -34,3 +20,46 @@ const handleSubmit = (e)=>{
     </div>
   )
 }
+
+// // 
+// <div className="App">
+// <div className='box'>
+//   <h1>Search Pokemon Name</h1>
+//   <input type='text' onChange={(e)=>{setPokemonName(e.target.value)}}></input>
+//   <button onClick={searchFunction}>Search</button>
+// </div>
+// {/* <Display/> */}
+// <div>{!searchEmpty ?(<h1>Nothing is chosen</h1>) :(<> <h1>{pokemonInfo.name}</h1>
+//   <h2>{pokemonInfo.hp}</h2>
+//   <h2>{pokemonInfo.attack}</h2>
+//   <img src={pokemonInfo.img} alt='Pokemon image'/>
+//   <h2>{pokemonInfo.type}</h2></>
+//   )}
+ 
+// </div>
+// </div>
+
+// const base_URL = 'https://pokeapi.co/api/v2/pokemon'
+// const [pokemonName, setPokemonName] = useState('')
+
+// const [pokemonInfo, setPokemonInfo]= useState({
+//   name: '',
+//   img: '',
+//   hp: '',
+//   attack: '',
+//   type:'',
+// })
+
+// // A use State to see if anything is being searched
+// const [searchEmpty, setSearchEmpty]= useState(false)
+
+// // Function for the button.
+// const searchFunction =()=>{
+//   Axios.get(`${base_URL}/${pokemonName}`).then((response)=>
+//     setPokemonInfo({name: pokemonName, img: response.data.sprites.front_default, hp: response.data.stats[0].base_stat,
+//     attack: response.data.stats[1].base_stat, type:response.data.types[0].type.name}))
+
+//     setSearchEmpty(true)
+//   }
+// //  Things to fix, the captialization of the search and the captialization of the names. Fix for card display, Fix for component searching.
+// // Fix for secondary typing search
