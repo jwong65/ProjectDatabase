71289d0d1d6aedb1458d5467c9730ef2f5a5c441
jwong65 <jason.wong6507@gmail.com>
2023-10-17 01:18:33 -0400

    Changed to reflect API testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+// import { useEffect } from 'react';
 import './sass/style.css'
+// import { useState } from 'react';
 
 function App() {
+ 
   return (
     <div className="App">
       <div className='box'>
-        This is a test for GH pages using a react model.
+        <p>This is a test for GH pages using a react model.</p>
+        <p>We will attempt to test an input field and see if it can access the database.</p>
       </div>
     </div>
   );
