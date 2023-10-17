09fa6d204d4f2331876fb83bfbbbcb20ec0d1d9d
jwong65 <jason.wong6507@gmail.com>
2023-10-17 02:45:29 -0400

    Added the form that will change based on the name inserted into the text but need to fix the json output

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,22 +4,33 @@ import '../sass/style.css'
 
 export default function Display() {
     
-const base_URL = 'https://pokeapi.co/api/v2/'
+const base_URL = 'https://pokeapi.co/api/v2/pokemon'
 const [pokedata, setData] = useState([]);
+const [pokemonName, setName]= useState()
 
 const fetchInfo =() =>{
-  fetch(base_URL)
+    console.log(`${base_URL}/${pokemonName}`)
+  fetch(`${base_URL}/${pokemonName}`)
   .then((res)=>res.json())
   .then((json)=> {
     console.log(json)
     setData(json)
   })
 }
+const handleSubmit = (e)=>{
+    e.preventDefault()
+    console.log(pokemonName)
+    fetchInfo();
+}
   return (
-    <div className='display'>Display
-    <button onClick={fetchInfo}> Test Fetch</button>
+    <div className='display'>
+        <form onSubmit={handleSubmit}>
+            <input type='text' onChange={(e)=> setName(e.target.value)}></input>
+            <input type='submit' value='Submit'></input>
+        </form>
+    {/* <button onClick={fetchInfo}> Test Fetch</button> */}
     <pre>
-    {JSON.stringify(pokedata, null, 2)}</pre>
+    {JSON.stringify(pokedata, null, 1)}</pre>
     </div>
   )
 }
